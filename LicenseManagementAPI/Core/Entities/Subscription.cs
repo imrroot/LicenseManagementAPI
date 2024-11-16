59bed379af641b1b038789625208c19d62a2a637
MrRoot <khatarbahman@gmail.com>
2024-11-16 11:33:28 +0330

    Subscription and relationships defined

---
+++ LicenseManagementAPI/Core/Entities/Subscription.cs
@@ -0,0 +1,11 @@
+﻿namespace LicenseManagementAPI.Core.Entities
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } //Access Level Name e.g., "Booster", "Premium"
+        public int Level { get; set; } //Integer of Access Level
+        public int AppId { get; set; } //Foreign Key to Application
+        public App App { get; set; }
+    }
+}
