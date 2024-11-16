b882a391a1fc691cd2897878bcc6738ed4abf9eb
MrRoot <khatarbahman@gmail.com>
2024-11-16 11:29:44 +0330

    User entity defined

---
+++ LicenseManagementAPI/Core/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+
+namespace LicenseManagementAPI.Core.Entities
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string OwnerId { get; set; } = GenerateShortId();
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string Email { get; set; }
+        public DateTime RegistrationDate { get; set; } = DateTime.UtcNow;
+        public ICollection<App> Apps { get; set; } = new List<App>();
+
+    }
+    public partial class User
+    {
+        //Generate Unique Secret as IV (16 Byte) of Requests Encryption 
+        private static string GenerateShortId()
+        {
+            using var rng = new RNGCryptoServiceProvider();
+            var data = new byte[12]; 
+            rng.GetBytes(data);
+            return Convert.ToBase64String(data).Substring(0, 16); 
+        }
+    }
+}
