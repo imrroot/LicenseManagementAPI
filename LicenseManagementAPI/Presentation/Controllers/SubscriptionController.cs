e0a60f44435709f8eeaafd86395d9a631610b945
MrRoot <khatarbahman@gmail.com>
2024-11-29 13:07:10 +0330

    +Updated SubscriptionCotroller

--- LicenseManagementAPI/Presentation/Controllers/SubscriptionController.cs
+++ LicenseManagementAPI/Presentation/Controllers/SubscriptionController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LicenseManagementAPI.Presentation.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [Route("api/subscription")]
     [ApiController]
     public class SubscriptionController : ControllerBase
@@ -22,24 +22,24 @@ namespace LicenseManagementAPI.Presentation.Controllers
         {
             if (!ModelState.IsValid) return BadRequest(ModelState);
             var userId = int.Parse(User.FindFirst("Id").Value); 
-            await _subscriptionService.AddSubscriptionAsync(addSubscriptionDto, userId);
-            return Ok(new { message = "Subscription added successfully" });
+            return await _subscriptionService.AddSubscriptionAsync(addSubscriptionDto, userId);
+           
         }
         [HttpPost("list/{id}")]
         public async Task<IActionResult> GetSubscription(int id)
         {
             if (!ModelState.IsValid) return BadRequest(ModelState);
             //var userId = int.Parse(User.FindFirst("Id").Value); 
-            var list = await _subscriptionService.GetSubscriptionsByAppIdAsync(id);
-            return Ok(list);
+            return (IActionResult)await _subscriptionService.GetSubscriptionsByAppIdAsync(id);
+           
         }
 
         [HttpDelete("delete/{id}")]
         public async Task<IActionResult> DeleteSubscription(int id)
         {
             var userId = int.Parse(User.FindFirst("Id").Value);
-             await _subscriptionService.DeleteSubscriptionAsync(id, userId);
-            return Ok(new { message = "Subscription deleted successfully" });
+            return await _subscriptionService.DeleteSubscriptionAsync(id, userId);
+           
         }
     }
 
