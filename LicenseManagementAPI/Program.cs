a7e087f8ddb297f20249e76e6af6796769590e38
MrRoot <khatarbahman@gmail.com>
2024-11-29 13:08:23 +0330

    +Updated Main Program

--- LicenseManagementAPI/Program.cs
+++ LicenseManagementAPI/Program.cs
@@ -1,4 +1,15 @@
 
+using System.Text;
+using LicenseManagementAPI.Application.Interfaces;
+using LicenseManagementAPI.Application.Services;
+using LicenseManagementAPI.Core.Interfaces;
+using LicenseManagementAPI.Infrastructure.Data;
+using LicenseManagementAPI.Infrastructure.Repositories;
+using LicenseManagementAPI.Presentation.Middleware;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
 namespace LicenseManagementAPI
 {
     public class Program
@@ -9,12 +20,47 @@ namespace LicenseManagementAPI
 
             // Add services to the container.
 
+            
+            builder.Services.AddDbContext<LicenseDbContext>(options =>
+                options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddAuthentication("Bearer")
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                    };
+                });
+            builder.Services.AddScoped<IUserService, UserService>();
+            builder.Services.AddScoped<IAppService, AppService>();
+            builder.Services.AddScoped<ILicenseService, LicenseService>();
+            builder.Services.AddScoped<ISubscriptionService, SubscriptionService>();
+            builder.Services.AddScoped<IEncryptionService, EncryptionService>(); // Ensure this is included
+
+            // Register Repositories
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IAppRepository, AppRepository>();
+            builder.Services.AddScoped<ILicenseRepository, LicenseRepository>();
+            builder.Services.AddScoped<ISubscriptionRepository, SubscriptionRepository>();
+
+
+            builder.Services.AddAuthorization();
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddAuthorization();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+
+            app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -25,6 +71,7 @@ namespace LicenseManagementAPI
 
             app.UseHttpsRedirection();
 
+            app.UseAuthorization();
             app.UseAuthorization();
 
 
