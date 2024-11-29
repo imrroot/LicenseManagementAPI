386de2c1d330b84fbf3ddc70d88d4c5bd954d583
MrRoot <khatarbahman@gmail.com>
2024-11-29 10:16:20 +0330

    -Custom Exception

---
+++ LicenseManagementAPI/Core/Exceptions/Exceptions.cs
@@ -0,0 +1,34 @@
+﻿namespace LicenseManagementAPI.Core.Exceptions;
+
+public class EntityDuplicateException : Exception
+{
+    public EntityDuplicateException(string message) : base(message) { }
+}
+public class EntityNotFoundException : Exception
+{
+    public EntityNotFoundException(string message) : base(message) { }
+}
+public class EntityValidationException : Exception
+{
+    public EntityValidationException(string message) : base(message) { }
+}
+public class DatabaseConstraintException : Exception
+{
+    public DatabaseConstraintException(string message, Exception innerException) 
+        : base(message, innerException) { }
+}
+public class ConcurrencyException : Exception
+{
+    public ConcurrencyException(string message) : base(message) { }
+}
+public class ResourceLimitExceededException : Exception
+{
+    public ResourceLimitExceededException(string message) : base(message) { }
+}
+public class DataIntegrityException : Exception
+{
+    public DataIntegrityException(string message) : base(message) { }
+}
+
+
+
