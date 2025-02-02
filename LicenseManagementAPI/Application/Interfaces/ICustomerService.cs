177eaed6166fc5d6f31afba5238a828bc1184a41
MrRoot <khatarbahman@gmail.com>
2025-02-02 09:34:18 +0330

    +Added Customer Interface

---
+++ LicenseManagementAPI/Application/Interfaces/ICustomerService.cs
@@ -0,0 +1,19 @@
+﻿using LicenseManagementAPI.Application.Services;
+using LicenseManagementAPI.Presentation.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+using LicenseManagementAPI.Core.Entities;
+
+namespace LicenseManagementAPI.Application.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task<IActionResult> CustomerValidateLicense(string licenseKey, string hwid, string ip,App app);
+        Task<IActionResult> CustomerLoginAsync(EncryptedRequestDto encryptedRequest);
+        Task<IActionResult> CustomerLicenseGetStatusAsync(EncryptedRequestDto encryptedRequest);
+        Task<IActionResult> CustomerRenewLicenseAsync(EncryptedRequestDto encryptedRequest);
+        Task<IActionResult> CustomerBanLicenseAsync(EncryptedRequestDto encryptedRequest);
+
+    }
+}
