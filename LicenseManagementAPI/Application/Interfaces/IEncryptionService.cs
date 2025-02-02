8a117f76cd58d1af80e42f8d29e6462b3058f06c
MrRoot <khatarbahman@gmail.com>
2025-02-02 09:35:03 +0330

    +Updated decrpyt method return Type to CustomerRequestBodyDto

--- LicenseManagementAPI/Application/Interfaces/IEncryptionService.cs
+++ LicenseManagementAPI/Application/Interfaces/IEncryptionService.cs
@@ -1,9 +1,11 @@
-﻿namespace LicenseManagementAPI.Application.Interfaces
+﻿using LicenseManagementAPI.Presentation.DTOs;
+
+namespace LicenseManagementAPI.Application.Interfaces
 {
     public interface IEncryptionService
     {
         string Encrypt(string plainText, string key, string iv);
-        string Decrypt(string cipherText, string key, string iv);
+        CustomerRequestBodyDto Decrypt(string cipherText, string key, string iv);
     }
 
 }
