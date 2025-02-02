38652c3d8fca3869d4884dd495bc5cfb1434cd1a
MrRoot <khatarbahman@gmail.com>
2025-02-02 09:35:42 +0330

    +Added Remove Device Method to LicenseService

--- LicenseManagementAPI/Application/Interfaces/ILicenseService.cs
+++ LicenseManagementAPI/Application/Interfaces/ILicenseService.cs
@@ -13,15 +13,13 @@ namespace LicenseManagementAPI.Application.Interfaces
         Task<IActionResult> UnfreezeLicenseAsync(string licenseKey,int userId);
         Task<IActionResult> CreateLicenseAsync(CreateLicenseDto createLicenseDto,int userId);
         Task<IActionResult> BanLicenseAsync(string licenseKey,int userId);
+        Task<IActionResult> UnBanLicenseAsync(string licenseKey, int userId);
         Task<IActionResult> GetLicenseStatusAsync(string licenseKey,int userId);
         Task<IActionResult> DeleteLicenseAsync(string licenseKey,int userId);
         Task<IActionResult> DeleteAllAsync(int appId, int userId, LicenseFilterType filterType = LicenseFilterType.All);
-        Task<IActionResult> CustomerValidateLicense(string licenseKey, string hwid, string ip);
-        Task<IActionResult> CustomerLoginAsync(EncryptedRequestDto encryptedRequest);
-        Task<IActionResult> GetCustomerLicenseStatusAsync(EncryptedRequestDto encryptedRequest);
-        Task<IActionResult> CustomerRenewLicenseAsync(EncryptedRequestDto encryptedRequest);
-        Task<IActionResult> CustomerBanLicenseAsync(EncryptedRequestDto encryptedRequest);
-        
+        Task<IActionResult> DeleteDeviceAsync(string licenseKey, int userId);
+
+
     }
 
 
